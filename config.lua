433a9ea3094ade3f4eabf39d06e48272cc045d04
pratiksaxena <pratik2709@gmail.com>
2017-09-13 02:33:43 +0530

    fix

--- config.lua
+++ config.lua
@@ -2,5 +2,13 @@
 local config = require("lapis.config")
 
 config("development", {
-  greeting = "Hello world"
+    mysql = {
+        host = "127.0.0.1",
+        port = 3306,
+        user = "root",
+        password = "root2709",
+        database = "lua_test"
+    }
 })
+
+return config
